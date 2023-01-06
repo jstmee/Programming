4d86e119f0f367edd0645c5a31c735166b20c518
Vishnu Kumar <jstmevishnu@gmail.com>
2023-01-06 23:58:18 +0530

    initial commit

---
+++ Contests/mytemplate.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#define int long long
+using namespace std;
+
+void myf(){
+    
+    
+    
+
+
+
+    
+    
+    
+
+
+}
+
+int32_t main(){
+    #ifndef ONLINE_JUDGE
+           freopen("input.txt","r",stdin);
+           freopen("output.txt","w",stdout);
+    #endif
+    int t;cin>>t;
+    while(t--){
+        myf();
+    }
+
+    return 0;
+}
