86085892f7c1ed57131faf01d1d5b9cc96766275
Vishnu Kumar <jstmevishnu@gmail.com>
2023-02-13 00:50:18 +0530

    changed

--- Contests/new.cpp
+++ Contests/new.cpp
@@ -1,7 +1,30 @@
 #include<bits/stdc++.h>
 #define int long long
 using namespace std;
+
 void myf(){
+    
+    int n;cin>>n;
+    string s;cin>>s;
+    int count=0;
+    int ans=INT_MAX;
+    for(int i=0;i<n;i++){
+        if(s[i]!='a' && s[i]!='e' && s[i]!='i' && s[i]!='o' && 
+        s[i]!='u'){
+            count++;
+            ans=max(ans,count);
+
+        }
+        else{
+            count=0;
+        }
+        
+    }
+    if(ans>=4){
+        cout<<"NO"<<endl;
+        return;
+    }
+    cout<<"YES"<<endl;
     
     
 
