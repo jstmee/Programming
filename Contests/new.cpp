dc6be24e58b9bb2c5258e4ff38c812bdc9d878db
Vishnu Kumar <jstmevishnu@gmail.com>
2023-03-13 18:51:49 +0530

    ignore files and done some more changes

--- Contests/new.cpp
+++ Contests/new.cpp
@@ -40,8 +40,8 @@ void myf(){
 }
 int32_t main(){
     #ifndef ONLINE_JUDGE
-           freopen("input.txt","r",stdin);
-           freopen("output.txt","w",stdout);
+           freopen("D:\\Programming\\C++ Programming\\input.txt","r",stdin);
+           freopen("D:\\Programming\\C++ Programming\\output.txt","w",stdout);
     #endif
     int t;
     cin>>t;
