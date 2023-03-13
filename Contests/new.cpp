d358d2209f87e6d4648d48a52c4a46ca033c1f78
Vishnu Kumar <jstmevishnu@gmail.com>
2023-03-13 18:35:35 +0530

    added modified

--- Contests/new.cpp
+++ Contests/new.cpp
@@ -4,52 +4,39 @@ using namespace std;
 
 void myf(){
     int n;cin>>n;
-    vector<pair<int,int>> v(n);
-    bool check=false;
-    int mini=INT32_MAX;
-    int maxi=INT32_MIN;
+    vector<int> v(n);
     for(int i=0;i<n;i++){
-        cin>>v[i].first;
-        v[i].second=i+1;
-        if(v[i].first==1){
-            check=true;
-        }
-        mini=min(mini,v[i].first);
-        maxi=max(maxi,v[i].first);
+        cin>>v[i];
     }
-
-    if(check){
-        if(mini==maxi){
-            cout<<0<<endl;
-            return;
+    vector<int> ans;
+    int p=1;
+    ans.push_back(1);
+    for(int i=0;i<n;i++){
+        if(v[i]==1){
+            
+            ans.push_back(p+1);
         }
         else{
-            cout<<-1<<endl;
-            return;
+            
+            ans.push_back(p);
         }
+    }
+    if(v[n-1]==(ans[0]+ans[n-1])%2){
+        cout<<"YES"<<endl;
+        return;
     }
     
-    int ans=0;
-    sort(v.begin(),v.end());
-    vector<pair<int,int>> ij;
-    while(v[0].first!=v[n-1].first){
-        if(v[n-1].first!=2){
-            v[n-1].first=ceil((double)v[n-1].first/v[0].first);
-            ans++;
-            ij.push_back({v[n-1].second,v[0].second});
-            sort(v.begin(),v.end());
-        }
-        else {
-            continue;
-        }
-    }
-    cout<<ans<<endl;
-    for(auto p:ij){
-        cout<<p.first<<" "<<p.second<<endl;
-    }
+    cout<<"NO"<<endl;
+    
+    
+
+
+    
 
 
 
+
+    
 }
 int32_t main(){
     #ifndef ONLINE_JUDGE
@@ -58,6 +45,7 @@ int32_t main(){
     #endif
     int t;
     cin>>t;
+    
     while(t--){
         myf();
     }
