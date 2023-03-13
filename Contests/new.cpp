f685394262b4247909b4b693287c6c5ddb938c07
Vishnu Kumar <jstmevishnu@gmail.com>
2023-03-13 19:00:07 +0530

    adding a code in recusion and dp

--- Contests/new.cpp
+++ Contests/new.cpp
@@ -3,38 +3,7 @@
 using namespace std;
 
 void myf(){
-    int n;cin>>n;
-    vector<int> v(n);
-    for(int i=0;i<n;i++){
-        cin>>v[i];
-    }
-    vector<int> ans;
-    int p=1;
-    ans.push_back(1);
-    for(int i=0;i<n;i++){
-        if(v[i]==1){
-            
-            ans.push_back(p+1);
-        }
-        else{
-            
-            ans.push_back(p);
-        }
-    }
-    if(v[n-1]==(ans[0]+ans[n-1])%2){
-        cout<<"YES"<<endl;
-        return;
-    }
     
-    cout<<"NO"<<endl;
-    
-    
-
-
-    
-
-
-
 
     
 }
