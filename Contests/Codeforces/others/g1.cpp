e6291131b97d860927c2c8f4cce1fe735581cc86
Vishnu Kumar <jstmevishnu@gmail.com>
2023-06-11 20:05:04 +0530

    done some changes

--- Contests/Codeforces/others/g1.cpp
+++ Contests/Codeforces/others/g1.cpp
@@ -2,50 +2,35 @@
 #define int long long
 using namespace std;
 
+
+
+
 void myf(){
-    int n,m,k;cin>>n>>m>>k;
-    vector<int> v1(n),v2(n);
-    for(int i=0;i<k;i++){
-        cin>>v1[i];
-    }
-    pair<int,int> myp={0,INT_MIN};
-    for(int i=0;i<k;i++){
-        cin>>v2[i];
-        if(myp.second<v2[i]){
-            myp.second=v2[i];
-            myp.first=v1[i];
+    vector<int> v={0,3,1,4,1,5,9};
+    vector<char> c={'A','B','C','D','E','F','G'};
+
+    char a,b;cin>>a>>b;
+    int p=a-65;
+    int q=b-65;
+    if(p<q){
+        int sum=0;
+        for(int i=p+1;i<=q;i++)
+        {
+            sum+=v[i];
+            
         }
+        cout<<sum<<endl;return;
     }
-    
-    vector<int> adj[n+1];
-    for(int i=0;i<m;i++){
-        int a,b;cin>>a>>b;
-        adj[a].push_back(b);
-        adj[b].push_back(a);
-    }
-    vector<int> dis(n+1,-1);
-    queue<int> q;
-    q.push(myp.first);
-    dis[myp.first]=1;
-    while(!q.empty()){
-        int node=q.front();
-        q.pop();
-        for(auto it:adj[node]){
-            if(dis[it]==-1){
-                dis[it]=dis[node]+1;
-            }
+    else{
+        int sum=0;
+        for(int i=q+1;i<=p;i++)
+        {
+            sum+=v[i];
+            
         }
+        cout<<sum<<endl;return;
+
     }
-    int maxdis=INT_MIN;
-    for(int i=1;i<=n;i++){
-        maxdis=max(maxdis,dis[i]);
-    }
-    if(maxdis<=myp.second){
-        cout<<"YES"<<endl;return;
-    }
-    
-    cout<<"NO"<<endl;return;
-    
 }
 int32_t main() {
 	// your code goes here
@@ -53,7 +38,8 @@ int32_t main() {
            freopen("D:\\Programming\\C++ Programming\\input.txt","r",stdin);
            freopen("D:\\Programming\\C++ Programming\\output.txt","w",stdout);
     #endif
-	int t;cin>>t;
+	int t=1;
+    // cin>>t;
 	while(t--){
 	    myf();
 	}
---
+++ MyLearning/1.DSA/C++/a.cpp
@@ -0,0 +1,22 @@
+// Associativity is not used in the below program.
+// Output is compiler dependent.
+
+#include <stdio.h>
+
+int x = 0;
+int f1()
+{
+	x = 5;
+	return x;
+}
+int f2()
+{
+	x = 10;
+	return x;
+}
+int main()
+{
+	int p = f1() + f2();
+	printf("%d ", x);
+	return 0;
+}
