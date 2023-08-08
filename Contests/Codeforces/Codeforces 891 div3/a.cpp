43f5d5ab9a4888e245dfe7e8e755f46fc97e61a3
Vishnu Kumar <jstmevishnu@gmail.com>
2023-08-08 20:23:34 +0530

    Added

--- Contests/Codeforces/Codeforces 891 div3/a.cpp
+++ Contests/Codeforces/Codeforces 891 div3/a.cpp
@@ -2,34 +2,24 @@
 #define int long long
 using namespace std;
 
-bool sortByVal(const pair<int, int>& a, const pair<int, int>& b){
-    if(a.first == b.first) return a.second < b.second;
-    
-    return a.first > b.first;
-}
+
 void myf(){
     int n;cin>>n;
     vector<int> v(n);
-    map<int,int> mp;
+    int sum=0;
     for(int i=0;i<n;i++){
         cin>>v[i];
-        mp[v[i]]++;
+        sum+=v[i];
     }
+    for(int i=0;i<n;i++){
+        if((v[i]%2==0) && ((sum-v[i])%2==0)  || (v[i]%2!=0) && ((sum-v[i])%2!=0)){
+            cout<<"YES"<<endl;return;
 
-    vector<pair<int,int>> tempans;
-    for(auto it:mp){
-        tempans.push_back({it.second,it.first});
-    }
-    sort(tempans.begin(),tempans.end(),sortByVal);
-    for(auto it:tempans){
-        for(int i=0;i<it.first;i++){
-            cout<<it.second<<" ";
         }
     }
-    cout<<endl;
+    cout<<"NO"<<endl;return;
 
-    
-    
+
 
 
 }
---
+++ Contests/Codeforces/Codeforces 891 div3/b.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+#define int long long
+using namespace std;
+
+//2 3 5 1
+//2 2 1 3 1 1
+//1 2 3 5
+//1 1 1 2 2 3
+
+//1 3 1
+
+//1= min(a,b)
+//3=min(c,d)
+//1=min(e,f)
+
+/*
+7 5 3 5 3 3=3+2+1=4
+ans=x,y,z,a
+
+*/
+
+void myf(){
+
+
+}
+
+
+
+int32_t main(){
+    #ifndef ONLINE_JUDGE
+           freopen("D:\\Programming\\C++ Programming\\input.txt","r",stdin);
+           freopen("D:\\Programming\\C++ Programming\\output.txt","w",stdout);
+    #endif
+    int t;
+    cin>>t;
+    
+    while(t--){
+        myf();
+    }
+    return 0;
+}
