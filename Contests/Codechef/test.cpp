2d0173fc357d8a29fd17dbf966c412bffffc5447
Vishnu Kumar <jstmevishnu@gmail.com>
2023-08-13 13:47:29 +0530

    added

--- Contests/Codechef/test.cpp
+++ Contests/Codechef/test.cpp
@@ -4,36 +4,7 @@ using namespace std;
 
 
 void myf(){
-    string s;cin>>s;
-    int a=0,b=0;
-    int n=s.size();
-    for(int i=0;i<(int)s.size();i++){
-        if(s[i]<'a'){
-            a++;
-        }
-        else{
-            b++;
-        }
-    }
 
-    if(a>b){
-        for(int i=0;i<n;i++){
-            s[i]=toupper(s[i]);
-
-        }
-        cout<<s<<endl;
-
-
-    }
-    else{
-
-        for(int i=0;i<n;i++){
-            s[i]=tolower(s[i]);
-
-        }
-        cout<<s<<endl;
-
-    }
 
 }
 
