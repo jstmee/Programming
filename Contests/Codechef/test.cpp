62ae1b6df734d898fb0f909c4584da4bdefc7eac
Vishnu Kumar <jstmevishnu@gmail.com>
2023-08-20 15:23:42 +0530

    added files in dev

--- Contests/Codechef/test.cpp
+++ Contests/Codechef/test.cpp
@@ -1,18 +1,52 @@
 #include<bits/stdc++.h>
 #define int long long
 using namespace std;
+// int ans=0;
 
+int ans = INT_MAX;
+void solve(vector<int> &temp, int n, int k, int sum, int i)
+{
+    if(sum>=ans){
+        return;
+    }
+    if (temp.size() == n)
+    {
+        // ans = min(ans, sum);
+        // bool check=false;
+        ans=min(ans,sum);
+        return;
+    }
+    for (int j = i; j <= 100; j++)
+    {
+        temp.push_back(j);
+        bool check=false;
+        for(int pp=0;pp<temp.size();pp++){
+            for(int qq=pp+1;qq<temp.size();qq++){
+                if(temp[pp]+temp[qq]==k){
+                    check=true;
+                    break;
+                }
+            }
+        }
+        if(check==false){
+            solve(temp, n, k, sum + j, temp.back()+1);
 
+        }
+        temp.pop_back();
+    }
+}
+int minimumSum(int n, int k)
+{
+    // vector<int> temp = {};
+    // int sum = 0;
+    // solve(temp, n, k, sum, 1);
+    // return ans;
+    
+}
 void myf(){
-    vector<int> myarr={3,4,5,4,3,6};
-    map<int,int> mp;
-    for(auto it:myarr){
-        mp[it]++;
-    }
-    for(auto it:mp){
-        cout<<it.first<<" "<<it.second<<endl;;
-    }
+    cout<<minimumSum(5,4)<<endl;
 
+    
 
 
 }
