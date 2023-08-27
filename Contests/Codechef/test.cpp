abf02331703082f6c9a4f4f5244da7016531af58
Vishnu Kumar <jstmevishnu@gmail.com>
2023-08-27 14:16:55 +0530

    added

--- Contests/Codechef/test.cpp
+++ Contests/Codechef/test.cpp
@@ -1,93 +1,38 @@
 #include<bits/stdc++.h>
-// #define int long long
+#define int long long
 using namespace std;
 
-vector<int> topSort(unordered_map<int,vector<int>> &adj,vector<int> &indegree){
-        queue<int> q;
-        for(int i=0;i<adj.size();i++){
-            if(indegree[i]==0){
-                q.push(i);
+long long minimumPossibleSum(int n, int target) {
+        
+        int sum=0;
+        int temp=1;
+        int count=0;
+        unordered_map<int,int> mp;
+        while(true){
+            int diff=target-temp;
+            if(mp.find(diff)!=mp.end()){
+                count++;
+                sum+=temp;
+                mp[temp]++;
             }
-        }
-        vector<int> ans;
-        while(!q.empty()){
-            int temp=q.front();
-            ans.push_back(temp);
-            q.pop();
-            for(auto it:adj[temp]){
-                indegree[it]--;
-                if(indegree[it]==0){
-                    q.push(it);
-                }
+            
+            temp++;
+            if(count==n){
+                break;
             }
+            
+            
+            
+            
         }
-        if(ans.size()==adj.size()){
-            return ans;
-        }
-        return {};
-    }
-
-vector<int> sortItems(int n, int m, vector<int>& group, vector<vector<int>>& beforeItems) {
-        for(int i=0;i<n;i++){
-            if(group[i]==-1){
-                group[i]=m;
-                m++;
-            }
-        }
-        unordered_map<int,vector<int>> itemsGraph;
-        vector<int> itemsIndegree(n,0);
-        for (int i = 0; i < n; i++) {
-            itemsGraph[i] = vector<int>();
-        }
-        unordered_map<int,vector<int>> groupGraph;
-        vector<int> groupIndegree(m,0);
-        for (int i = 0; i < m; ++i) {
-            groupGraph[i] = vector<int>();
-        }
-        for(int i=0;i<n;i++){
-            for(auto it:beforeItems[i]){
-                itemsGraph[it].push_back(i);
-                itemsIndegree[i]++;
-                if(group[i]!=group[it]){
-                    groupGraph[group[it]].push_back(group[i]);
-                    groupIndegree[group[i]]++;
-                }
-            }
-        }
-        vector<int> itemsOrder=topSort(itemsGraph,itemsIndegree);
-        vector<int> groupOrder=topSort(groupGraph,groupIndegree);
-        unordered_map<int,vector<int>> groupToItemsInOrder;
-        vector<int> ans;
-        for(auto it:itemsOrder){
-            groupToItemsInOrder[group[it]].push_back(it);
-        }
-        for(auto it:groupOrder){
-            auto temp=groupToItemsInOrder[it];
-            for(auto el:temp){
-                ans.push_back(el);
-            }
-        }
-        return ans;
+        
+        
+        return sum;
+        
     }
 
 void myf(){
-    vector<int> a={-1,-1,1,0,0,1,0,-1};
-    vector<vector<int>> b={
-        {},
-        {6},
-        {5},
-        {6},
-        {3,6},
-        {},
-        {},
-        {}
-    };
-    auto it=sortItems(8,2,a,b);
-    for(auto p:it){
-        cout<<p<<" ";
-    }
-
-
+    cout<<minimumPossibleSum(3,3)<<endl;
 }
 
 
---
+++ Contests/asksenior/c++ basis/1.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#define int long long
+using namespace std;
+
+
+void myf(){
+
+    string s;cin>>s;
+    cout<<"Hello, "<<s<<endl;
+
+
+}
+
+
+
+int32_t main(){
+    #ifndef ONLINE_JUDGE
+           freopen("D:\\Programming\\C++ Programming\\input.txt","r",stdin);
+           freopen("D:\\Programming\\C++ Programming\\output.txt","w",stdout);
+    #endif
+    int t=1;
+    // cin>>t;
+    
+    while(t--){
+        myf();
+    }
+    return 0;
+}
