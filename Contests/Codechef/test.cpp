0aaee466e7473ffd530a5779be12b79f565eba25
Vishnu Kumar <jstmevishnu@gmail.com>
2023-09-02 15:29:13 +0530

    added

--- Contests/Codechef/test.cpp
+++ Contests/Codechef/test.cpp
@@ -6,70 +6,24 @@ using namespace std;
 1  2  3 4 5  6  7 8 9 10 11 12 13 14 15 16 17 18 19
 */
 void myf(){
-    string s1,s2;cin>>s1>>s2;
-    if(s1[0]!=s2[0]){
-        cout<<"NO"<<endl;
-        return;
-    }
-    // int check=0;
-    int oneind=s1.size();
-    for(int i=1;i<s1.size()-1;i++){
-        if(s1[i]==s2[i] && s1[i]=='1'){
-            oneind=i;
-            break;
-        }
-    }
-    string p1=s1,p2=s2;
-    for(int i=oneind;i<s1.size();i++){
-        p1[i]='1';
-        p2[i]='1';
-    }
-    if(p1==p2){
-        cout<<"YES"<<endl;return;
-    }
-    int zeroind=-1;
-    for(int i=s1.size()-2;i>=1;i--){
-        if(s1[i]==s2[i] && s1[i]=='0'){
-            zeroind=i;
-            break;
-        }
-    }
-    string q1=s1,q2=s2;
-    for(int i=0;i<=zeroind;i++){
-        q1[i]='0';
-        q2[i]='0';
-    }
-    if(q1==q2){
-        cout<<"YES"<<endl;return;
-    }
+    string a,b;cin>>a>>b;
 
-    if(zeroind <oneind){
-        for (int i = 0; i <= zeroind; i++)
-        {
-            p1[i] = '0';
-            p2[i] = '0';
+    for(int i=1;i<a.size();i++){
+        if(a[i-1]=='0' && a[i]=='1' && b[i-1]=='0' && b[i]=='1'){
+            cout<<"YES"<<endl;return;
         }
-        if (p1 == p2)
-        {
-            cout << "YES" << endl;
-            return;
-        }
-        for (int i = oneind; i < s1.size(); i++)
-        {
-            q1[i] = '1';
-            q2[i] = '1';
-        }
-        if (q1 == q2)
-        {
-            cout << "YES" << endl;
-            return;
-        }
+        // cout<<"NO"<<endl;return;
     }
+    cout<<"NO"<<endl;return;
+
+
+    
+    
     
 
 
 
-    cout<<"NO"<<endl;return;
+    
 
 
 
