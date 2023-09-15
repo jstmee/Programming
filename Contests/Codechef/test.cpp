ccd7feae24cc4e05e656f4c5052e1383172c477c
Vishnu Kumar <jstmevishnu@gmail.com>
2023-09-15 15:24:37 +0530

    added

--- Contests/Codechef/test.cpp
+++ Contests/Codechef/test.cpp
@@ -2,18 +2,54 @@
 #define int long long
 using namespace std;
 
-//m+x*p<=n
-void myf(){
-    int n,m,p;cin>>n>>m>>p;
-    int ans=0;
-    for(int i=0;i<=n;i++){
 
-        if(m+i*p<=n){
-            ans++;
+void myf(){
+
+    int n;cin>>n;
+    vector<int> v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+    vector<vector<int>> ans(n);
+    ans[0].push_back(v[0]);
+    for(int i=1;i<n;i++){
+
+        for(auto it:ans[i-1]){
+            int ai=it;
+            int temp1=v[i]+ai;
+            int temp2=-v[i]+ai;
+            // if(temp1>=0){
+            //     ans[i].push_back(temp1);
+            // }
+            // if(temp2>=0){
+            //     ans[i].push_back(temp2);
+            // }
+            if(temp1==temp2){
+                ans[i].push_back(temp1);
+                continue;
+            }
+            if(temp1>=0){
+                ans[i].push_back(temp1);
+            }
+            if(temp2>=0){
+                ans[i].push_back(temp2);
+            }
+
+        }
+        if(ans[i].size()>=2){
+            cout<<-1<<endl;return;
         }
     }
-    cout<<ans<<endl;return;
+   
+    for(auto it:ans){
+        cout<<it[0]<<" ";
+        
+    }
 
+    cout<<endl;
+
+    
+    
 
 
 }
@@ -25,8 +61,8 @@ int32_t main(){
            freopen("D:\\Programming\\C++ Programming\\input.txt","r",stdin);
            freopen("D:\\Programming\\C++ Programming\\output.txt","w",stdout);
     #endif
-    int t=1;
-    // cin>>t;
+    int t;
+    cin>>t;
     
     while(t--){
         myf();
