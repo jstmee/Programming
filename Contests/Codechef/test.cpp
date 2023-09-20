a8774222e15597317790b8b611bdec38fa62943e
Vishnu Kumar <jstmevishnu@gmail.com>
2023-09-20 13:46:04 +0530

    added

--- Contests/Codechef/test.cpp
+++ Contests/Codechef/test.cpp
@@ -4,53 +4,34 @@ using namespace std;
 
 
 void myf(){
-
     int n;cin>>n;
-    vector<int> v(n);
+    string s;cin>>s;
+    vector<int> v;
+    int sum=0;
     for(int i=0;i<n;i++){
-        cin>>v[i];
-    }
-    vector<vector<int>> ans(n);
-    ans[0].push_back(v[0]);
-    for(int i=1;i<n;i++){
-
-        for(auto it:ans[i-1]){
-            int ai=it;
-            int temp1=v[i]+ai;
-            int temp2=-v[i]+ai;
-            // if(temp1>=0){
-            //     ans[i].push_back(temp1);
-            // }
-            // if(temp2>=0){
-            //     ans[i].push_back(temp2);
-            // }
-            if(temp1==temp2){
-                ans[i].push_back(temp1);
-                continue;
-            }
-            if(temp1>=0){
-                ans[i].push_back(temp1);
-            }
-            if(temp2>=0){
-                ans[i].push_back(temp2);
-            }
-
+        if(s[i]=='L'){
+            v.push_back((n-i-1)-i);
+            sum+=i;
         }
-        if(ans[i].size()>=2){
-            cout<<-1<<endl;return;
+        else{
+            v.push_back(i-(n-i-1));
+            sum+=n-i-1;
         }
     }
-   
-    for(auto it:ans){
-        cout<<it[0]<<" ";
-        
+    sort(v.begin(),v.end(),greater<int> ());
+    for(auto it:v){
+        if(it>0){
+            sum+=it;
+        }
+        cout<<sum<<" ";
     }
-
     cout<<endl;
-
-    
     
 
+
+    
+
+    
 
 }
 
@@ -61,7 +42,7 @@ int32_t main(){
            freopen("D:\\Programming\\C++ Programming\\input.txt","r",stdin);
            freopen("D:\\Programming\\C++ Programming\\output.txt","w",stdout);
     #endif
-    int t;
+    int t=1;
     cin>>t;
     
     while(t--){
