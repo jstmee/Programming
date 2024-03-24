60a36e892a22fcf276e11c7e371e323b9295ad4b
jstmee <jstmevishnu@gmail.com>
2024-03-24 22:47:39 +0530

    edited files

--- Contests/Codechef/test.cpp
+++ Contests/Codechef/test.cpp
@@ -1,60 +1,37 @@
 #include<bits/stdc++.h>
+
 #define int long long
 using namespace std;
 
+/*
 
-void myf(){
-    int n,k;cin>>n>>k;
-    // cout<<n<<k<<endl;
-    vector<int> v(n);
-    int mod=1e9+7;
-    for(int i=0;i<n;i++){
-        cin>>v[i];
-    }
-    int ans=0;
-    int sum=0;
-    for(int i=0;i<n;i++){
-        int tt=max((sum+v[i])%mod,v[i]%mod);
-        sum=tt;
-        ans=max(ans%mod,sum%mod);
-    }
-    // cout<<ans<<endl;
-    // int p=1;
-    int pp=ans;
-    int nans=0;
-    for(int i=0;i<k;i++){
-        // nans+=(pp);
-        nans=(nans+pp)%mod;
-        pp=(pp+nans)%mod;
-    }
-    // cout<<nans<<endl;
-    int tsum=0;
-    for(auto it:v){
-        // tsum+=it;
-        tsum=(tsum+it)%mod;
-    }
+
+
+*/
+void myf() {
+
+
     
-    cout<<((nans%mod+tsum%mod)%mod+mod)%mod<<endl;
-
-
-
-
-
-
 }
 
+int32_t main() {
+
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL);
 
 
-int32_t main(){
+
     #ifndef ONLINE_JUDGE
-           freopen("D:\\Programming\\C++ Programming\\input.txt","r",stdin);
-           freopen("D:\\Programming\\C++ Programming\\output.txt","w",stdout);
+        freopen("D:\\Programming\\C++ Programming\\input.txt","r",stdin);
+        freopen("D:\\Programming\\C++ Programming\\output.txt","w",stdout);
     #endif
+
     int t;
-    cin>>t;
-    
-    while(t--){
+    t=1;
+    // cin>>t;
+    while(t--) {
         myf();
     }
+
     return 0;
 }
