7ac6fbc34b9d3cd456e3cca9d84bb1d36af429d5
jstmee <jstmevishnu@gmail.com>
2024-04-13 13:42:42 +0530

    edited some files

--- Contests/Codechef/test.cpp
+++ Contests/Codechef/test.cpp
@@ -5,11 +5,80 @@ using namespace std;
 
 /*
 
+given beuatifull arrray a1 a2...
+
+return min no of element to remove for not it to beuatufull
+
+5-3
+2
+
+5
+1 2 1 2 1
 
 
 */
 void myf() {
 
+    int n;cin>>n;
+    vector<int> v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+    if(n==1){
+        cout<<-1<<endl;
+        return;
+    }
+    if(n==2){
+        if(v[0]!=v[1]){
+            cout<<0<<endl;
+            return;
+        }
+        else{
+            cout<<-1<<endl;
+            return;
+        }
+    }
+
+    if((v[0]!=v[1] and v[0]!=v[2]) or (v[n-1]!=v[n-2] and v[n-1]!=v[n-3])){
+        cout<<0<<endl;
+        return;
+    }
+
+    int ans=INT_MAX;
+    for(int i=0;i<n-1;i++){
+        if(i!=0 and v[i]!=v[i-1]){
+            // v[i]=v[i-1];
+            ans=min(ans,i);
+            break;
+        }
+    }
+    for(int i=n-1;i>=1;i--){
+        if(i!=n-1 and v[i]!=v[i+1]){
+            ans=min(ans,n-i-1);
+        }
+    }
+    if(ans==INT_MAX){
+        cout<<-1<<endl;
+        return;
+    }
+    cout<<ans<<endl;
+
+
+
+    
+
+
+
+    
+
+
+
+
+
+
+    
+    
+    
 
     
 }
@@ -28,7 +97,7 @@ int32_t main() {
 
     int t;
     t=1;
-    // cin>>t;
+    cin>>t;
     while(t--) {
         myf();
     }
